6788d3eb57e8590fea69ee04201c8d9ba0dbe6ce
Elena Fisak <elena.fisak94@gmail.com>
2026-09-19 15:59:44 +0200

    refactor: use promises to retrieve data in public routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,104 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getAllBooks = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(books);
+    } catch (error) {
+      reject({ message: error.message });
+    }
+  });
+};
+
+const getBookByIsbn = (isbn) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (isbn) {
+        const book = books[isbn];
+
+        if(!book) {
+          reject({ message: "Book not found" });
+        }
+
+        resolve(book);
+      }
+
+      reject({ message: 'No ISBN provided' })
+    } catch (error) {
+      reject({ message: error.message });
+    }
+  });
+}
+
+const getBookByAuthor = (author) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (author) {
+        const lowercaseAuthor = author.toLowerCase();
+        const booksList = [];
+        for(const book of Object.values(books)) {
+          if(book.author.toLowerCase() === lowercaseAuthor) {
+            booksList.push(book);
+          }
+        }
+
+        if(!booksList.length) {
+          reject({ message: "Books not found" });
+        }
+
+        resolve(booksList);
+      }
+
+      reject({ message: 'No author provided' })
+    } catch (error) {
+      reject({ message: error.message });
+    }
+  });
+}
+
+const getBookByTitle = (title) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (title) {
+        const lowercaseTitle = title.toLowerCase();
+
+        const book = Object.values(books).find(item => item.title.toLowerCase() === lowercaseTitle);
+
+        if(!book) {
+          reject({ message: "Book not found" });
+        }
+
+        resolve(book);
+      }
+
+      reject({ message: 'No title provided' })
+    } catch (error) {
+      reject({ message: error.message });
+    }
+  });
+}
+
+const getReviewsByIsbn = (isbn) => {
+  return new Promise((resolve, reject) => {
+    try {
+      if(isbn) {
+        const book = books[isbn];
+
+        if(!book) {
+          reject({ message: "Book not found" });
+        }
+
+        resolve(book.reviews);
+      }
+
+      reject({ message: 'No ISBN provided' })
+    } catch (error) {
+      reject({ message: error.message });
+    }
+  })
+}
+
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -27,84 +125,55 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 2));
+  getAllBooks().then((books) => {
+    res.status(200).json(books);
+  }).catch((error) => {
+    res.status(500).json({ message: error.message });
+  });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
 
-  if(isbn) {
-    const book = books[isbn];
-
-    if(!book) {
-      return res.status(404).json({ message: "Book is not found" });
-    } else {
-      return res.status(200).send(JSON.stringify(book, null, 2));
-    }
-  }
-
-  return res.status(400).json({ message: "No ISBN provided" });
+  getBookByIsbn(isbn).then((book) => {
+    res.status(200).json(book);
+  }).catch((error) => {
+    res.status(500).json({ message: error.message });
+  })
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
 
-  if(author) {
-    const lowercaseAuthor = author.toLowerCase();
-    const booksList = [];
-
-    for(const book of Object.values(books)) {
-      if(book.author.toLowerCase() === lowercaseAuthor) {
-        booksList.push(book);
-      }
-    }
-
-    if(!booksList.length) {
-      return res.status(404).json("Books by this author not found");
-    } else {
-      return res.status(200).send(JSON.stringify(booksList, null, 2));
-    }
-  }
-
-  return res.status(400).json("No author provided");
+  getBookByAuthor(author).then((book) => {
+    res.status(200).json(book);
+  }).catch((error) => {
+    res.status(500).json({ message: error.message });
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
 
-  if(title) {
-    const lowercaseTitle = title.toLowerCase();
-
-    const book = Object.values(books).find(item => item.title.toLowerCase() === lowercaseTitle);
-
-    if(!book) {
-      return res.status(404).json({ message: "Book with this title not found" });
-    } else {
-      return res.status(200).send(JSON.stringify(book, null, 2));
-    }
-  }
-
-  return res.status(400).json({ message: "No title provided" });
+  getBookByTitle(title).then((book) => {
+    res.status(200).json(book);
+  }).catch((error) => {
+    res.status(500).json({ message: error.message });
+  });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
 
-  if(isbn) {
-    const book = books[isbn];
-
-    if(!book) {
-      return res.status(404).json({ message: "Book is not found" });
-    } else {
-      return res.status(200).send(JSON.stringify(book.reviews, null, 2));
-    }
-  }
-
-  return res.status(400).json({ message: "No ISBN provided" });
+  getReviewsByIsbn(isbn).then((reviews) => {
+    res.status(200).json(reviews);
+  }).catch((error) => {
+    res.status(500).json({ message: error.message });
+  })
 });
 
 module.exports.general = public_users;
