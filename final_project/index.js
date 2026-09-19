55883d860e8fce5303d637047d33d5ed3f636343
Elena Fisak <elena.fisak94@gmail.com>
2026-09-19 15:22:29 +0200

    feat: implement adding and deleting review

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-    if(req.session.authorization){
+app.use("/customer/auth/*", function auth(req, res, next){
+    if(req.session && req.session.authorization){
         let token = req.session.authorization['accessToken'];
 
         jwt.verify(token, 'access', (error, user) => {
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -28,7 +28,7 @@ regd_users.post("/login", (req,res) => {
 
   if(authenticatedUser(username, password)){
     let accessToken = jwt.sign({
-      data: password,
+      username: username
     }, 'access', {expiresIn: 60 * 60 });
 
     req.session.authorization = {
@@ -43,8 +43,50 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+
+  if(!isbn){
+    return res.status(400).json({ message: "No ISBN provided" });
+  }
+
+  const book = books[isbn];
+
+  if(!book) {
+    return res.status(404).json({ message: "No book found with this ISBN" });
+  }
+
+  const review = req.body.review;
+
+  if(!review){
+    return res.status(400).json({ message: "No review provided" });
+  }
+
+  const username = req.user.username;
+
+  book.reviews[username] = review;
+
+  return res.status(200).json({ message: 'Successfully added review' });
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+
+  if(!isbn){
+    return res.status(400).json({ message: "No ISBN provided" });
+  }
+
+  const book = books[isbn];
+
+  if(!book) {
+    return res.status(404).json({ message: "No book found with this ISBN" });
+  }
+
+  const username = req.user.username;
+
+  delete book.reviews[username];
+
+  return res.status(200).json({ message: 'Successfully deleted review' });
 });
 
 module.exports.authenticated = regd_users;
